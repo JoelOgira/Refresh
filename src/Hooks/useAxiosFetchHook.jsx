97692a75e8a0d8af02d07d056cbb9e31240bc2ea
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-16 16:30:47 +0300

    Created Hooks folder

---
+++ src/Hooks/useAxiosFetchHook.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useAxiosFetchHook = () => {
+    return (
+    <div>useAxiosFetchHook</div>
+    )
+}
+
+export default useAxiosFetchHook;
