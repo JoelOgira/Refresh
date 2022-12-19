f08ca52a684697e5ea1a1f8125fbf5ed66e74c41
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-19 22:41:40 +0300

    Created hook for fetching data

--- src/Hooks/useAxiosFetchHook.jsx
+++ src/Hooks/useAxiosFetchHook.jsx
@@ -1,10 +1,47 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useAxiosFetchHook = () => {
-    return (
-    <div>useAxiosFetchHook</div>
-    )
+const useAxiosFetchHook = (dataUrl) => {
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        let isMounted = true;
+        const source = axios.CancelToken.source();
+
+        const fetchData = async (url) => {
+            setIsLoading(true);
+            try {
+                const response = await axios.get(url, {
+                    cancelToken: source.token
+                });
+                if(isMounted) {
+                    setData(response.data);
+                    setError(null);
+                }
+            } catch (err) {
+                if (isMounted) {
+                    setError(err.message);
+                    setData([]);
+                }
+            } finally {
+                isMounted && setIsLoading(false);
+            }
+        }
+
+        fetchData(dataUrl);
+
+        const cleanUp = () => {
+            isMounted = false;
+            source.cancel
+        }
+
+        cleanUp;
+
+    }, [dataUrl])
+
+    return {data, error, isLoading}
 }
 
 export default useAxiosFetchHook;
