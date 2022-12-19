85d237e06f4ead256df8877e3536061c83a8e717
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-19 22:42:49 +0300

    Added new functionality

--- src/Context/DataContext.jsx
+++ src/Context/DataContext.jsx
@@ -1,12 +1,20 @@
-import { useContext, useState, useEffect, createContext } from "react";
-
+import { useState, useEffect, createContext } from "react";
+import useAxiosFetchHook from "../Hooks/useAxiosFetchHook";
 
 const DataContext = createContext({});
 
 export const DataProvider = ({children}) => {
+
+  const [bikes, setBikes] = useState([]);
+  const { data, error, isLoading } = useAxiosFetchHook('http://localhost:8000/bikes');
+
+  useEffect(() => {
+    setBikes(data);
+  }, [data])
+
   return (
     <DataContext.Provider value={{
-        
+        bikes, setBikes, error, isLoading
     }}>
         {children}
     </DataContext.Provider>
