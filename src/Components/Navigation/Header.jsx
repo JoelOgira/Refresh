fab15ad3fb6482990ea9f8f6a8d0de0af66258a4
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-19 22:37:12 +0300

    Added new functionality

---
+++ src/Components/Navigation/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <nav className='bg-primary top-0 z-50 sticky py-5 mb-3'>
+            <div className="flex justify-between items-center flex-wrap md:w-3/4 container mx-auto">
+                <h1 className='text-white text-4xl font-semibold'>
+                    Refresh
+                </h1>
+                <div className="inline-flex space-x-4">
+                    <Link to="/bikes" className="hover:text-secondary">
+                        Bikes
+                    </Link>
+                    <Link to="/cats" className="hover:text-secondary">
+                        Cats
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
