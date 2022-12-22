f95db26ad105af700af266685d9f7192b385109f
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-22 23:38:14 +0300

    Created input form and removed error area

--- src/Components/Bikes/Bikes.jsx
+++ src/Components/Bikes/Bikes.jsx
@@ -1,5 +1,4 @@
 import { useState, useContext } from "react";
-import { useParams } from "react-router-dom";
 import DataContext from "../../Context/DataContext";
 import api from "../../api/baseUrl"
 
@@ -7,31 +6,34 @@ const Bikes = () => {
   const { bikes, setBikes, error, isLoading } = useContext(DataContext);
     
   const [motorBike, setMotorBike] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorArea, setErrorArea] = useState(true);
-  const { id } = useParams();
-  const updatedBike = bikes.find((bike) => bike.id == id);
 
-  const addBike = async () => {
+  const addBike = async (e) => {
+    e.preventDefault();
     const id = bikes.length ? bikes[bikes.length - 1 ].id + 1 : 1;
     const newBike = {id, bikeName:motorBike, completed: false}
 
     try {
-      const response = await api.post('/bikes', newBike);
-      if (motorBike === "") {
-        setMessage("You cannot add an empty Item !!!")
-        setErrorArea(false);
-      } else {
-        setBikes([...bikes, response.data]);
-        setMotorBike('');
-      }
+      const response = await api.post('/bikes', newBike);      
+      setBikes([...bikes, response.data]);
+      setMotorBike('');      
     } catch (err) {
       console.log(err.message);
     }    
   }
 
   const markedBike = async (id) => {
-    
+    // let toUpdate = (bikes || []).find(i => i?.id === id);
+
+    // setBikes((data) => (
+    //   [
+    //     {
+    //       ...(toUpdate || {}),
+    //       completed: !(toUpdate?.completed) 
+    //     },
+    //     ...(bikes || []).filter(bike => bike?.id !== id),
+    //   ]
+    // ))
+    setBikes((bikes || []).map(bike => bike?.id === id ? {...bike, completed: !bike?.completed} : bike ))
   }
 
   const deleteBike = async (id) => {
@@ -52,7 +54,7 @@ const Bikes = () => {
                 <li className={` ${(!bike.completed) ? 'bg-gray-200' : 'bg-secondary'} rounded-full px-4  flex flex-row justify-between my-3`} key={bike.id}>
                 <p className='align-baseline py-4'>{bike.bikeName} </p>
                 <div className='flex flex-row space-x-4 my-2'>
-                    <button onClick={() => markedBike(bike.id, `${!bike.completed}`)} className={`py-2 px-4 rounded-full bg-gray-400 hover:bg-primary hover:text-white`}>Marked</button>
+                    <button onClick={() => markedBike(bike.id)} className={`py-2 px-4 rounded-full bg-gray-400 hover:bg-primary hover:text-white`}>Marked</button>
                     <button onClick={() => deleteBike(bike.id)} className='bg-primary py-2 px-4  rounded-full cursor-pointer text-white hover:bg-red-500'>Delete</button>
                 </div>
                 </li>
@@ -61,23 +63,21 @@ const Bikes = () => {
             }
         </ul>
 
-        <input 
-            type="text"
-            className="p-3 px-4 rounded-full bg-gray-400 placeholder-black text-white focus:outline-none"
-            placeholder="Add Bike"
-            value={motorBike}
-            onChange={e => setMotorBike(e.target.value)}
-        />    
-        
-        <button className='bg-primary text-white p-3 mx-4 my-3 rounded-full px-5 hover:bg-gray-400 hover:text-black' onClick={addBike}>
-            Add Bike
-        </button>
-        <div className={`${errorArea ? 'hidden' : 'flex flex-row'} space-x-6`}>
-            <p className="text-2xl text-red-600 py-3">
-            {`${message}`}
-            </p>
-            <button onClick={() => setErrorArea(!errorArea)} className='bg-green-500 px-4 rounded-xl hover:bg-green-100'>OK</button>
-        </div>
+        <form onSubmit={addBike}>
+          <input 
+              type="text"
+              className="p-3 px-4 rounded-full bg-gray-400 placeholder-black text-white focus:outline-none"
+              placeholder="Add Bike"
+              required
+              value={motorBike}
+              onChange={e => setMotorBike(e.target.value)}
+          />    
+          
+          <button type="submit" className='bg-primary text-white p-3 mx-4 my-3 rounded-full px-5 hover:bg-gray-400 hover:text-black'>
+              Add Bike
+          </button>
+        </form>
+
     </div>
   )
 }
