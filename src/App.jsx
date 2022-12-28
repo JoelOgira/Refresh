056fe203ff9776d6b73bc9d01b84b889aeb9aeb3
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-28 20:57:45 +0300

    Added catFacts route

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import { DataProvider } from './Context/DataContext';
 import Header from './Components/Navigation/Header';
 import Footer from './Components/Navigation/Footer';
 import Bikes from './Components/Bikes/Bikes';
+import CatFacts from './Components/CatFacts/CatFacts';
 
 function App() {
   
 
   return (
     <div className="App h-full bg-primary px-5">      
-      <Header />
-      <DataProvider>
-        <Routes>
-          <Route path='/bikes' element={<Bikes />} />
-        </Routes>
-      </DataProvider>
-      <Footer />      
+    <Header />
+    <DataProvider>
+      <Routes>
+        <Route path='/bikes' element={<Bikes />} />
+        <Route path='/cats' element={<CatFacts />} />
+      </Routes>
+    </DataProvider>
+    <Footer />      
     </div>
   )
 }
