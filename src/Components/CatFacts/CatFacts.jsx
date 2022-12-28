0b75937007f144bde5d6bff6a3776e209b4c9644
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-28 21:02:02 +0300

    created the functionality to fetch cat facts

---
+++ src/Components/CatFacts/CatFacts.jsx
@@ -0,0 +1,39 @@
+import axios from "axios"
+import { useState,useEffect } from "react"
+
+const CatFacts = () => {
+    const [catFact, setCatFact] = useState("");
+
+    const fetchCatFacts = async () => {
+        try {
+            const response = await axios.get(`https://catfact.ninja/fact`);
+            setCatFact(response.data.fact);
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+    useEffect(() => {
+        fetchCatFacts();
+    }, [])
+
+    return (
+    <div className="container overflow-y-auto grow mx-auto md:w-3/4">
+        
+        <h1 className="text-4xl text-center mb-6 underline font-semibold">
+            Cat Facts
+        </h1>
+        
+        <div className="flex flex-wrap w-3/4 h-96 rounded-lg shadow-black bg-gray-400 p-4 mx-auto">
+
+            <div className="mx-auto">
+                <button onClick={fetchCatFacts} className="bg-primary p-3 px-5 rounded-full">Fetch Cat Facts</button>
+            </div>
+
+            <p className="py-6 text-center">{catFact}</p>
+            
+        </div>
+    </div>
+    )
+}
+
+export default CatFacts;
