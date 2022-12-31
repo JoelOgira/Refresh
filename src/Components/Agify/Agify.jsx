0762481f04c38203652587e191883af04be6789d
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-31 06:00:55 +0300

    Created agify folder

---
+++ src/Components/Agify/Agify.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Agify = () => {
+    const [bio, setBio] = useState(``);
+    const [name, setName] = useState(``);
+
+    const predictAge = async () => {
+        try {
+            const response = await axios.get(`https://api.agify.io/?name=${name}`);
+            setBio(response.data);
+            setName('');
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    useEffect(() => {
+        predictAge();
+    }, []);
+
+    return (
+        <div className="container overflow-y-auto grow mx-auto md:w-3/4">
+            <input 
+                type="text"
+                className="p-3 px-4 rounded-full bg-gray-400 placeholder-black text-white focus:outline-none"
+                placeholder="Enter Name"
+                required
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />    
+      
+            <button onClick={predictAge} type="submit" className='bg-primary border border-gray-400 text-white p-3 mx-4 my-3 rounded-full px-5 hover:bg-gray-400 hover:text-black'>
+                Predict Age
+            </button>
+
+            <div className="bg-primary">
+                <p className="my-2">Name: <span className="text-secondary">{bio.name}</span></p>
+                <p className="my-2">Age: <span className="text-secondary">{bio.age}</span></p>
+            </div>
+        </div>
+    )
+}
+
+export default Agify;
