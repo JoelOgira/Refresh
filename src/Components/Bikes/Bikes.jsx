0eed89313ad340970aa6c97fc05ec771c240b048
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-04 17:42:04 +0300

    Made changes

--- src/Components/Bikes/Bikes.jsx
+++ src/Components/Bikes/Bikes.jsx
@@ -23,8 +23,7 @@ const Bikes = () => {
     }    
   }
 
-  const markedBike = async (id) => {
-    
+  const markedBike = async (id) => {    
     setBikes((bikes || []).map(bike => bike?.id === id ? {...bike, completed: !bike?.completed} : bike ))
   }
 
@@ -44,9 +43,9 @@ const Bikes = () => {
   return (
   <div className="card container overflow-y-auto grow mx-auto md:w-3/4">
 
-    {isLoading && <p className="text-center text-4xl my-11">The requested bikes are loading</p>}
+    {isLoading && <p className="text-center text-white text-4xl my-11">The requested bikes are loading...</p>}
 
-    {!isLoading && error && {error}}
+    {!isLoading && error && <p className="text-center text-red-500 text-4xl my-11">{error}</p>}
 
     {!isLoading && !error && 
     
@@ -55,11 +54,11 @@ const Bikes = () => {
       <ul className='my-5'>
           {
           bikes.length ?
-          (bikes.map(bike => (
+          ((bikes || []).map(bike => (
               <li className={` ${(!bike?.completed) ? 'bg-gray-200' : 'bg-secondary'} rounded-full px-4  flex flex-row justify-between my-3`} key={bike.id}>
-              <p className='align-baseline py-4'>{bike.bikeName} </p>
+              <p className='align-baseline py-4'>{bike?.bikeName} </p>
               <div className='flex flex-row space-x-4 my-2'>
-                  <button onClick={() => markedBike(bike.id)} className={`py-2 px-4 rounded-full bg-gray-400 hover:bg-primary hover:text-white`}>Marked</button>
+                  <button onClick={() => markedBike(bike?.id)} className={`py-2 px-4 rounded-full bg-gray-500 hover:bg-primary hover:text-white`}>Marked</button>
                   <button onClick={handleModal} className='bg-primary py-2 px-4  rounded-full cursor-pointer text-white hover:bg-red-500'>Delete</button>
               </div>
               </li>
@@ -71,14 +70,14 @@ const Bikes = () => {
       <form onSubmit={addBike}>
         <input 
             type="text"
-            className="p-3 px-4 rounded-full bg-gray-400 placeholder-black text-white focus:outline-none"
+            className="p-3 px-4 rounded-full bg-gray-500 placeholder-black text-white focus:outline-none"
             placeholder="Add Bike"
             required
             value={motorBike}
             onChange={e => setMotorBike(e.target.value)}
         />    
         
-        <button type="submit" className='bg-primary border border-gray-400 text-white p-3 mx-4 my-3 rounded-full px-5 hover:bg-gray-400 hover:text-black'>
+        <button type="submit" className='bg-primary border border-gray-500 text-white p-3 mx-4 my-3 rounded-full px-5 hover:bg-gray-500 hover:text-black'>
             Add Bike
         </button>
       </form>
--- src/Components/CatFacts/CatFacts.jsx
+++ src/Components/CatFacts/CatFacts.jsx
@@ -1,8 +1,10 @@
 import axios from "axios"
 import { useState,useEffect } from "react"
+import useToggle from "../../Hooks/useToggle";
 
 const CatFacts = () => {
     const [catFact, setCatFact] = useState("");
+    const {state:isVisible, toggle} = useToggle();
 
     const fetchCatFacts = async () => {
         try {
@@ -17,13 +19,13 @@ const CatFacts = () => {
     }, [])
 
     return (
-    <div className="container overflow-y-auto grow mx-auto md:w-3/4">
+    <div className="container overflow-y-auto grow mx-auto md:w-3/4 text-white">
         
         <h1 className="text-4xl text-center mb-6 underline font-semibold">
             Cat Facts
         </h1>
         
-        <div className="flex flex-col flex-wrap w-3/4 h-96 rounded-lg shadow-black bg-gray-400 p-4 mx-auto">
+        <div className="flex flex-col flex-wrap w-3/4 h-96 rounded-lg shadow-black bg-gray-500 p-4 mx-auto my-6">
 
             <div className="mx-auto mt-3 text-white">
                 <button onClick={fetchCatFacts} className="bg-primary p-3 px-5 rounded-full">Fetch Cat Facts</button>
@@ -32,6 +34,17 @@ const CatFacts = () => {
             <p className="py-3 text-center">{catFact}</p>
             
         </div>
+
+        <div className="flex flex-col mx-auto items-center space-y-6 md:space-y-0">
+            <button 
+                className="border-2 p-3 px-6 rounded-full "
+                onClick={toggle}
+            >
+                {isVisible? 'Hide Text' : 'Show Text'}
+            </button>
+            {isVisible && <p className="text-center">This is the text</p>}
+        </div>
+
     </div>
     )
 }
