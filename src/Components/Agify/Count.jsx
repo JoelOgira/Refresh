767ddaea5f274c2b2b406c2402a5db53285609b1
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-05 15:17:10 +0300

    Added functionality to the count component

---
+++ src/Components/Agify/Count.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Count = () => {
+    return (
+        <div className='container overflow-y-auto grow mx-auto md:w-3/4'>
+            <div className="flex flex-row">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Count;
