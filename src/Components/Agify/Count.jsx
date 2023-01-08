5ce440c05fb52bc91e45d1d69e12d220d86887c2
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-08 22:43:05 +0300

    Created and added functionality to Count component

--- src/Components/Agify/Count.jsx
+++ src/Components/Agify/Count.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import useCounter from "../../Hooks/useCounter";
 
 const Count = () => {
+
+    const { counter, increaseCounter, decreaseCounter, resetCounter } = useCounter();
+
     return (
         <div className='container overflow-y-auto grow mx-auto md:w-3/4'>
-            <div className="flex flex-row">
-                
+            <div className="flex flex-row justify-between space-x-6">
+                <div>
+                    {counter.count}
+                </div>
+                <div>
+                    <button
+                        onClick={increaseCounter}
+                    >
+                        increase
+                    </button>
+                </div>
+                <div>
+                    <button
+                        onClick={decreaseCounter}
+                    >
+                        decrease
+                    </button>
+                </div>
+                <div>
+                    <button
+                        onClick={resetCounter}
+                    >
+                        reset
+                    </button>
+                </div>
             </div>
         </div>
     )
