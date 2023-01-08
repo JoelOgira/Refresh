43c29518be3e204bcb3009113eae0080f29d6acf
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-08 22:46:03 +0300

    Created home component

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="text-white container overflow-y-auto grow mx-auto md:w-3/4">
+            
+        </div>
+    )
+}
+
+export default Home;
