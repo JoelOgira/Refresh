90dceca61d3b9d67900aca5e9ccf724d3d78de03
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-21 21:41:12 +0300

    Changed styling in the footer component

--- src/Components/Navigation/Footer.jsx
+++ src/Components/Navigation/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
     return (
         
-        <footer className='py-5 mt-3 sticky bg-primary
+        <footer className='py-8 mt-3 sticky bg-primary
         inset-x-0 top-full'>
             <div className="flex justify-between flex-wrap w-full container mx-auto md:w-3/4">
                 <div>
