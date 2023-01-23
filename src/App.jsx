259923ea224048b7dcc485c22f12261e58286059
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-23 10:07:45 +0300

    Added superheroes components for rendering

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import Agify from './Components/Agify/Agify';
 import Excuser from './Components/Excuser/Excuser';
 import Count from './Components/Agify/Count';
 import Home from './Components/Home/Home';
+import Superheroes from './Components/Superheroes/Superheroes';
+import RQSuperheroes from './Components/Superheroes/RQSuperheroes';
 
 function App() {
   
 
   return (
-    <div className="App h-full bg-primary px-5">      
+    <div className="App h-full bg-primary px-5 text-white">      
     <Header />
     <DataProvider>
       <Routes>
@@ -23,6 +25,8 @@ function App() {
         <Route path='/agify' element={<Agify />} />
         <Route path='/excuser' element={<Excuser />} />
         <Route path='/counter' element={<Count />} />
+        <Route path='/superheroes' element={<Superheroes />} />
+        <Route path='/rqsuperheroes' element={<RQSuperheroes />} />
       </Routes>
     </DataProvider>
     <Footer />      
