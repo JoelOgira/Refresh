17803dcb2d2790d75b914e194189e62bb36b7ac1
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-29 21:49:52 +0300

    Added items to home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,9 @@
 const Home = () => {
     return (
         <div className="text-white container overflow-y-auto grow mx-auto md:w-3/4">
-            
+            <h1 className="text-center uppercase text-4xl py-6 font-extrabold">
+                This is the homepage
+            </h1>
         </div>
     )
 }
